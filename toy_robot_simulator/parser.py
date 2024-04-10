81e1c394f4ac81b150f5348c9ac975bc23a05b2f
donfiguerres <lyndonfiguerres@gmail.com>
2024-04-10 13:51:01 +0800

    Add docstring

--- toy_robot_simulator/parser.py
+++ toy_robot_simulator/parser.py
@@ -12,9 +12,41 @@ from toy_robot_simulator.domain.position import Direction, Position
 
 
 class ParsedCommand:
-    """Parsed command"""
+    """Representation of a parsed command.
+
+    A line of command has the following format:
+
+    `COMMAND [ARG1, ARG2, ...]`
+
+    If the command does not have any arguments, the value `args` this field is
+    None. If the command has arguments, the value of this field is a dictionary
+    where the keys are the argument names and the values are the argument
+    values.
+
+    For example, consider the following `PLACE` command.
+
+    `PLACE 0,0,NORTH`
+
+    The corresponding `ParsedCommand` object will have the following fields:
+    - command: `Command.PLACE`
+    - args: `{'position': Position(0, 0, Direction.NORTH)}`
+
+    On the other hand, consider the following `MOVE` command.
+
+    `MOVE`
+
+    The corresponding `ParsedCommand` object will have the following fields:
+    - command: `Command.MOVE`
+    - args: None
+    """
 
     def __init__(self, command: Command, args: Optional[Dict[str, Any]] = None):
+        """Instantiate a parsed command.
+
+        Args:
+            command: The parsed command.
+            args: The arguments of the command.
+        """
         self.command = command
         self.args = args
 
@@ -23,10 +55,10 @@ def parse_line(command_line: str) -> ParsedCommand:
     """Parse a line of input
 
     Args:
-        command_line: The command line to parse
+        command_line: The command line to parse.
 
     Returns:
-        A parsed command
+        A parsed command.
     """
     try:
         command, *unparsed_args = command_line.split(maxsplit=1)
